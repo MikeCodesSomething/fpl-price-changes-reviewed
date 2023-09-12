152e1a71c0d8833d6a924f97aee82fc626fb78b7
MikeCodesSomething <129166471+MikeCodesSomething@users.noreply.github.com>
2023-09-12 12:29:14 +0100

    Add page styling Hide less interesting data by default

--- script.js
+++ script.js
@@ -30,20 +30,37 @@ fetch("https://docs.google.com/spreadsheets/d/e/2PACX-1vRbLHukC_yBzeaturQs00p2le
         columns.forEach(column => {
           const cell = document.createElement(index === 0 ? "th" : "td");
           cell.textContent = rowData[column];
+          cell.setAttribute('data-cell',dataObjects[0][column]);
           row.appendChild(cell);
         });
 
         table.appendChild(row);
-      });
+      }
+      )
 
+    // Hide default hidden rows
+    toggleHiddenRows([3,4,9,10]);
     // Highlight the highest value in specified rows after the data is imported
     highlightHighestValues([1,6,7,12]);
     // Highlight the lowest value in specified rows after the data is imported
     highlightLowestValues([2,3,4,5,8,9,10,11]);
+    //Add roles to table DOM elements
+    AddTableARIA();
+
   })
   .catch(error => {
     console.error("Error fetching Google Sheets data: ", error);
   });
+
+function toggleHiddenRows(inputRows) {
+  const table = document.getElementById("data-table");
+  const rows = table.getElementsByTagName("tr");
+  console.log(rows);
+  for (let i = 0; i < inputRows.length; i++) {
+    rows[inputRows[i]].classList.toggle("hidden")
+  }
+
+}
 
 function highlightHighestValues(inputRows) {
   const table = document.getElementById("data-table");
@@ -92,3 +109,42 @@ function highlightLowestValues(inputRows) {
     }
   }
 }
+
+
+//For accessibility we add roles to the table with a standard script.
+function AddTableARIA() {
+  try {
+    var allTables = document.querySelectorAll('table');
+    for (var i = 0; i < allTables.length; i++) {
+      allTables[i].setAttribute('role','table');
+    }
+    var allCaptions = document.querySelectorAll('caption');
+    for (var i = 0; i < allCaptions.length; i++) {
+      allCaptions[i].setAttribute('role','caption');
+    }
+    var allRowGroups = document.querySelectorAll('thead, tbody, tfoot');
+    for (var i = 0; i < allRowGroups.length; i++) {
+      allRowGroups[i].setAttribute('role','rowgroup');
+    }
+    var allRows = document.querySelectorAll('tr');
+    for (var i = 0; i < allRows.length; i++) {
+      allRows[i].setAttribute('role','row');
+    }
+    var allCells = document.querySelectorAll('td');
+    for (var i = 0; i < allCells.length; i++) {
+      allCells[i].setAttribute('role','cell');
+    }
+    var allHeaders = document.querySelectorAll('th');
+    for (var i = 0; i < allHeaders.length; i++) {
+      allHeaders[i].setAttribute('role','columnheader');
+    }
+    // this accounts for scoped row headers
+    var allRowHeaders = document.querySelectorAll('th[scope=row]');
+    for (var i = 0; i < allRowHeaders.length; i++) {
+      allRowHeaders[i].setAttribute('role','rowheader');
+    }
+  } catch (e) {
+    console.log("AddTableARIA(): " + e);
+  }
+}
+
